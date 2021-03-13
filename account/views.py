49076a46e9a1dcf0f60b5a399b0c185cdc33aa0a
Deepanshu721 <deepanshu.agg19@example.com>
2021-03-13 20:01:49 +0530

    Customized User Sign Up Form

--- account/views.py
+++ account/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from .forms import CustomSignUpForm
 
 # Create your views here.
 def signup_view(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomSignUpForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = CustomSignUpForm()
 
     context = {'form':form}
     return render(request, 'account/signup.html', context)
